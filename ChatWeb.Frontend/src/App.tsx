3e80a6af66d7c62013ba0e4d4e343d6fe8b325d4
João Paulo <joaopauloppeixoto@gmail.com>
2026-01-25 20:47:27 -0500

    Chore: installing ShadcnUI.

--- ChatWeb.Frontend/src/App.tsx
+++ ChatWeb.Frontend/src/App.tsx
@@ -1,14 +1,19 @@
-import './App.css'
-
 import { Link, Route, Routes } from 'react-router-dom'
 import { About, Home } from './pages'
+import { Button } from './components/ui/button'
+import { Separator } from './components/ui/separator'
 
 function App() {
-
   return (
     <>
-      <nav>
-        <Link to="/">Home</Link> | <Link to="/about">About</Link>
+      <nav className='flex flex-row gap-2'>
+        <Link to="/">
+          <Button className='cursor-pointer'>Home</Button>
+        </Link>
+        <Separator orientation='vertical' />
+        <Link to="/about">
+          <Button className='cursor-pointer'>About</Button>
+        </Link>
       </nav>
 
       <Routes>
--- ChatWeb.Frontend/vite.config.ts
+++ ChatWeb.Frontend/vite.config.ts
@@ -1,3 +1,4 @@
+import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
@@ -8,4 +9,10 @@ export default defineConfig({
     react(),
     tailwindcss()
   ],
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
